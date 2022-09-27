b0e50b4e8da2585116b94122ac6c8e689dee5058
Keitey <hkmdev22@gmail.com>
2022-09-26 23:23:05 -0300

    ajuste style card pokemon

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,14 +5,23 @@ export const CardContainer = styled.div`
     grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
     column-gap: 3rem;
     row-gap: 4rem;
-    margin: 1rem;
+    margin: 3rem;
 `;
 
 export const Card = styled.div`
     text-align: center;
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    border-radius: 3px;
+
+    img{
+        border-radius: 3px 3px 0px 0px;
+    }
 `;
 
 export const CardText = styled.div`
-    background: #868E96;
+    background: #424656;
+    border-radius: 0px 0px 3px 3px;
+    padding: 0.3rem;
+    color: #fff;
+    font-size: 18px;
 `;
