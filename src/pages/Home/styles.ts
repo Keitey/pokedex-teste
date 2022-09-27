87703d2220132fc1c480463854edaae093ecf0f8
Keitey <hkmdev22@gmail.com>
2022-09-27 19:56:13 -0300

    ajuste espacamento entre pokemons

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  column-gap: 9rem;
+  column-gap: 2.1rem;
   row-gap: 4rem;
   margin: 5rem 4rem;
   padding-bottom: 3rem;
