ca95e4cc42755c4a83f987b16d5abadfbdba9e4c
Keitey <hkmdev22@gmail.com>
2022-10-12 10:57:37 -0300

    get details pokemons

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,20 +1,39 @@
-import { useState, useEffect} from 'react'
-import { useParams} from 'react-router-dom'
-import { pokeName, getPokemons, getAbilities } from "../../services/api";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getAbilities, getPokemons } from "../../services/api";
 
 const Details = () => {
-  const { id } = useParams()
+  const { id }: any = useParams();
+  const [abilities, setAbilites] = useState<any[]>([])
   const [pokemons, setPokemons] = useState({});
 
-  useEffect(()=> {
-
-  }, [id])
+  useEffect(() => {
+    async function fetchAbilities(data: any){
+      const abilitiesData = await getAbilities(data.abilities)
+      setAbilites(abilitiesData)
+      console.log(abilitiesData)
+    }
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const pokemons = {
+          id,
+          image: data.sprites.other.home.front_default,
+          name: data.name,
+          moves: data.moves,
+          types: data.types,
+          abilities: data.abilities,
+        };;
+        setPokemons(pokemons);
+        fetchAbilities(data)
+      });
+  }, [id]);
 
   return (
     <div>
       <h1>página de detalhes</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
