179ecf34af34c0b0cd2944c12a4ca77ea0c1904a
Keitey <hkmdev22@gmail.com>
2022-10-12 16:17:20 -0300

    criando estilos

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,37 +1,56 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getAbilities, getPokemons } from "../../services/api";
+import { getAbilities } from "../../services/api";
+
+import * as C from "./styles"
 
 const Details = () => {
   const { id }: any = useParams();
   const [abilities, setAbilites] = useState<any[]>([])
-  const [pokemons, setPokemons] = useState({});
+  const [pokemon, setPokemon] = useState({
+      name: "",
+      image: "",
+      moves: [],
+      types: []
+  });
 
   useEffect(() => {
     async function fetchAbilities(data: any){
       const abilitiesData = await getAbilities(data.abilities)
       setAbilites(abilitiesData)
+      console.log(abilitiesData)
     }
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
       .then((res) => res.json())
       .then((data) => {
-        const pokemons = {
+        const pokemon = {
           id,
           image: data.sprites.other.home.front_default,
           name: data.name,
-          moves: data.moves,
+          moves: data.moves.slice(0,3),
           types: data.types,
-          abilities: data.abilities,
         };;
-        setPokemons(pokemons);
+        setPokemon(pokemon);
         fetchAbilities(data)
+        console.log(pokemon)
       });
   }, [id]);
 
   return (
-    <div>
-      <h1>página de detalhes</h1>
-    </div>
+    <>
+    <C.PokeDetails>
+      <C.Poke>
+      <span>nome do pokemon</span>
+      <img src={pokemon.image} alt="" />
+      </C.Poke>
+      <C.Description>
+        <span>moves:</span>
+        <span>Habilidades:</span>
+        <span>Tipo do pokemon:</span>
+      </C.Description>
+
+    </C.PokeDetails>
+    </>
   );
 };
 
---
+++ src/pages/Details/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const PokeDetails = styled.div`
+  display: flex;
+  gap: 3rem;
+  margin: 2rem;
+`;
+
+export const Poke = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
