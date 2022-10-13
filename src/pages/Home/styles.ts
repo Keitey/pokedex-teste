46335bd1c7ecb13ef75354d4177df53d4542d39d
Keitey <hkmdev22@gmail.com>
2022-10-13 15:42:44 -0300

    ajuste style page home

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,7 +5,7 @@ export const Container = styled.div`
   grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   column-gap: 2.1rem;
   row-gap: 4rem;
-  margin: 2.5rem 4rem;
+  margin: 2rem 4rem;
   padding-bottom: 1rem;
 `;
 
@@ -21,7 +21,7 @@ export const Button = styled.button`
   background: #feca1b;
   color: #3761a8;
   font-weight: bold;
-  padding: 1.3rem;
+  padding: 1rem;
   width: 150px;
   cursor: pointer;
 
