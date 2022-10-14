5f660767311ab214f522f32cc737668c81cb5a40
Keitey <hkmdev22@gmail.com>
2022-10-14 15:02:30 -0300

    armazenado no local storage estado de dark e ligh mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import { useState } from "react";
 import RoutesApp from "./routes";
 import GlobalStyle from "./styles/global";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, DefaultTheme } from "styled-components";
 import light from "./styles/themes/light";
 import dark from "./styles/themes/dark";
 import NavBar from "./components/NavBar/NavBar";
+import usePersistentState from "./utils/usePersistedState";
 
 const App = () => {
-  const [theme, setTheme] = useState(light);
+  const [theme, setTheme] = usePersistentState<DefaultTheme>("theme", light);
 
   const toggleTheme = () => {
     setTheme(theme.title === "light" ? dark : light);
   };
   return (
     <ThemeProvider theme={theme}>
-      <div>
+      <>
         <NavBar toggleTheme={toggleTheme} />
         <GlobalStyle />
         <RoutesApp />
-      </div>
+      </>
     </ThemeProvider>
   );
 };
